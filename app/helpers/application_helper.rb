ff0d19abc9cdb63f487354c95f1531e77ffb5104
Tim McCarthy <omegoshi@gmail.com>
2020-04-03 23:42:58 +0200

    change: move to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_link_tree_url(user)
+    if Rails.env.development?
+      "#{request.host}:#{request.port}/#{user.username}"
+    else
+      "#{request.host}/#{user.username}"
+    end
+  end
 end
