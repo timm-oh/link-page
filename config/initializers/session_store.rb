5d7607c1a681fa6abd5f8d3a2dbf8c995c4f0950
Tim McCarthy <omegoshi@gmail.com>
2020-04-11 15:58:55 +0200

    change: session things

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_link_page_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: "_link_page_#{Rails.env}", domain: :all
