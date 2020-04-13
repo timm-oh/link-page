3141fb95e23244d853cc914602a40b723cb086c3
Tim McCarthy <omegoshi@gmail.com>
2020-04-13 16:36:26 +0200

    change: move to file

--- app/controllers/acme_challenges_controller.rb
+++
@@ -1,9 +0,0 @@
-class AcmeChallengesController < ApplicationController
-  skip_before_action :authenticate_user!
-
-  def show
-    acme_challenges = JSON.parse(ENV['ACME_CHALLENGES'])
-
-    render plain: acme_challenges[params[:passcode]]
-  end
-end
