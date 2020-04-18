1954e27142f2fa0b3a76fad9b1072548ecf559bd
Tim McCarthy <omegoshi@gmail.com>
2020-04-18 14:37:07 +0200

    change: wrap in if statement

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,8 @@
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN'] || 'https://foo:bar@sentry.io/link-page'
-  config.current_environment = ENV['SENTRY_ENV']
-  config.release = ENV['HEROKU_SLUG_COMMIT']
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+if ENV['SENTRY_DSN']
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+    config.current_environment = ENV.fetch('SENTRY_ENV') { 'N/A' }
+    config.release = ENV.fetch('HEROKU_SLUG_COMMIT') { 'N/A' }
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
 end
